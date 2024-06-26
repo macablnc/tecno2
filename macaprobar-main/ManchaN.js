6ef458e62315d5da668401e3ffdbabddd10e32e9
macablnc <128642420+macablnc@users.noreply.github.com>
2024-06-26 19:12:42 -0300

    Add files via upload

---
+++ macaprobar-main/ManchaN.js
@@ -0,0 +1,59 @@
+class ManchaN {
+    constructor(img, x, y, w, h, velocidad) {
+     this.img = img;
+     this.x = x;
+     this.y = y;
+     this.w = w;
+     this.h = h;
+     this.velocidad = velocidad;
+     this.angulo = 0;
+     this.rotando = false;
+     this.rotacionInicial = random(TWO_PI); // Rotación inicial aleatoria
+     this.opacidad = 0; // Opacidad inicial
+     this.apareciendo = true; // Marcar como apareciendo
+    }
+   
+    startRotating() {
+     this.rotando = true;
+    }
+   
+    stopRotating() {
+     this.rotando = false;
+    }
+   
+    rotar() {
+     if (this.rotando) {
+      this.angulo += this.velocidad;
+     }
+    }
+   
+    desvanecer() {
+     if (this.opacidad > 0) {
+      this.opacidad -= 5; // Reducir opacidad
+     }
+    }
+   
+    aparecer() {
+     if (this.opacidad < 255) {
+      this.opacidad += 15; // Aumentar velocidad de opacidad
+      if (this.opacidad >= 255) {
+       this.apareciendo = false; // Dejar de aparecer al alcanzar opacidad máxima
+      }
+     }
+    }
+   
+    dibujar(capa) {
+     if (this.apareciendo) {
+      this.aparecer();
+     }
+     capa.push();
+     capa.translate(this.x + this.w / 2, this.y + this.h / 2);
+     this.rotar();
+     capa.rotate(this.angulo);
+     capa.rotate(this.angulo + this.rotacionInicial); // Aplicar rotación inicial aleatoria
+     capa.tint(255, this.opacidad); // Aplicar opacidad
+     capa.image(this.img, -this.w / 2, -this.h / 2, this.w, this.h);
+     capa.pop();
+    }
+   }
+   
---
+++ macaprobar-main/sketch.js
@@ -0,0 +1,200 @@
+let CapamanchaG;
+let CapamanchaN;
+let Capalineas;
+let Capafondo;
+let fondo; // Variable para la imagen de fondo
+let cant = 5;
+let manchaG = [];
+let manchaN = [];
+let lineas = [];
+let tiempoDentroCapa = 0;
+let tiempoAnterior = 0;
+let capaActual = "";
+let limiteImagenes = 5;
+let manchasG = [];
+let manchasN = [];
+let manchasLineas = [];
+let estado = "";
+
+// Configuración de volumen y audio
+let amp_min = 0.01;
+let amp_max = 0.2;
+let audioContext;
+let mic;
+let pitch;
+let amp;
+let haySonido = false;
+let antesHabiaSonido = false;
+let empezoElSonido = false;
+let gestorAmp;
+let amortiguacion = 0.9;
+
+let tiempoInicioSonido = 0; // Variable para registrar el inicio del sonido
+let duracionSonido = 0; // Variable para registrar la duración del sonido
+
+function preload() {
+  fondo = loadImage("data/Lienzo2.png"); // Cargar la imagen de fondo
+
+  // Cargar imágenes de manchas y líneas
+  for (let i = 0; i < cant; i++) {
+    let dmanchaG = "data/manchasg" + (i + 1) + ".png";
+    let dmanchaN = "data/manchasn" + (i + 1) + ".png";
+    let dlineas = "data/Linea" + (i + 1) + ".png";
+
+    manchaG[i] = loadImage(dmanchaG);
+    manchaN[i] = loadImage(dmanchaN);
+    lineas[i] = loadImage(dlineas);
+  }
+}
+
+function setup() {
+  let canvas = createCanvas(300, 550);
+  canvas.parent('sketch-container'); // Attach canvas to div
+
+  // Inicializar capas gráficas para p5.Graphics
+  Capafondo = createGraphics(300, 550);
+  Capafondo.image(fondo, 0, 0, 300, 550);
+
+  CapamanchaG = createGraphics(300, 550);
+  CapamanchaN = createGraphics(300, 550);
+  Capalineas = createGraphics(300, 550);
+
+  // Attach click event listener to canvas
+  canvas.mouseClicked(startAudio);
+}
+
+function startAudio() {
+  audioContext = getAudioContext();
+  audioContext.resume().then(() => {
+    mic = new p5.AudioIn();
+    mic.start(startPitch);
+  });
+}
+
+function startPitch() {
+  pitch = ml5.pitchDetection('./model/', audioContext, mic.stream, modelLoaded);
+}
+
+function modelLoaded() {
+  getPitch();
+}
+
+function draw() {
+  background(200);
+
+  image(Capafondo, 0, 0);
+
+  let tiempoTranscurrido = millis() - tiempoAnterior;
+  tiempoAnterior = millis();
+
+  actualizarCapa(tiempoTranscurrido);
+
+  // Manejar las manchas para cada capa
+  if (capaActual === "N") {
+    manejarManchas(manchasN, ManchaN, manchaN, 250, 400);
+  } else if (capaActual === "L") {
+    manejarManchas(manchasLineas, Linea, lineas,80, 100);
+  } else if (capaActual === "G") {
+    manejarManchas(manchasG, ManchaG, manchaG, 250, 450);
+  }
+
+  // Dibujar las manchas en sus respectivas capas
+  dibujarManchas(CapamanchaN, manchasN);
+  dibujarManchas(CapamanchaG, manchasG);
+  dibujarManchas(Capalineas, manchasLineas);
+
+  // Dibujar las capas en el canvas principal
+  image(CapamanchaG, 0, 0);
+  image(CapamanchaN, 0, 0);
+  image(Capalineas, 0, 0);
+
+  // Mostrar la duración del sonido
+  fill(0);
+  textSize(16);
+  text('Duración del sonido: ' + duracionSonido.toFixed(2) + ' segundos', 10, height - 10);
+
+  antesHabiaSonido = haySonido; // Guardar estado del fotograma anterior
+}
+
+function actualizarCapa(tiempoTranscurrido) {
+  let nuevaCapa = "";
+
+  if (pitch) {
+    pitch.getPitch(function(err, frequency) {
+      if (frequency > 90 && frequency < 245) {
+        nuevaCapa = "N";
+      } else if (frequency > 350) {
+        nuevaCapa = "L";
+      } else if (frequency > 250 && frequency < 345) {
+        nuevaCapa = "G";
+      }
+
+      if (nuevaCapa !== capaActual) {
+        capaActual = nuevaCapa;
+        tiempoDentroCapa = 0; // Resetear el tiempo al cambiar de capa
+      } else {
+        tiempoDentroCapa += tiempoTranscurrido;
+      }
+    });
+  }
+}
+
+function manejarManchas(manchas, ClaseMancha, imagenes, minSize, maxSize) {
+
+
+  if (manchas.length < limiteImagenes) {
+    if (manchas.length === 0 || tiempoDentroCapa === 0) {
+      let i = floor(random(cant));
+      let w = random(minSize, maxSize);
+      let h = random(minSize, maxSize);
+      let x = random(0, width-5); // Generar posición aleatoria sin límites
+      let y = random(0, height-5); // Generar posición aleatoria sin límites
+      let nuevaMancha = new ClaseMancha(imagenes[i], x, y, w, h);
+      manchas.push(nuevaMancha);
+    }
+  }
+}
+
+function dibujarManchas(capa, manchas) {
+  capa.clear();
+  for (let mancha of manchas) {
+    mancha.dibujar(capa);
+  }
+}
+
+// Función para manejar el resultado del clasificador de sonido
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  label = results[0].label;
+}
+
+// Esta función se llama cuando se redimensiona la ventana del navegador
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function getPitch() {
+  pitch.getPitch(function(err, frequency) {
+    if (err) {
+      console.error(err);
+    } else {
+      // Actualizar haySonido según la frecuencia detectada
+      haySonido = (frequency > 0);
+      
+      if (haySonido && !antesHabiaSonido) {
+        tiempoInicioSonido = millis(); // Registrar el tiempo cuando el sonido empieza
+      }
+
+      if (haySonido) {
+        duracionSonido = (millis() - tiempoInicioSonido) / 1000; // Actualizar duración del sonido en segundos
+      } else {
+        duracionSonido = 0;
+      }
+    }
+    // Llamar a getPitch de nuevo para mantener la detección continua
+    getPitch();
+  });
+}
